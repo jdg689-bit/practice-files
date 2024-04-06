733e64be92c20815115c664277c2b7b750b05694
Jacob de Graaf <jdgraaf689@gmail.com>
2024-04-06 10:17:00 +1000

    changes to file editor, trying to make JSON requests

--- file-editor/back-end/server.js
+++ file-editor/back-end/server.js
@@ -9,12 +9,42 @@ const server = http.createServer((req, res) => {
     if (req.url === '/createFile') {
         createFile(req, res);
     }
+    else if (req.url === '/submit-form') {
+        createUser(req, res);
+    }
 });
 
 function createFile(req, res) {
     fs.writeFile(path.join(__dirname, 'test.txt'), 'Test successful!', (err) => {
         if (err) throw err;
     });    
+}
+
+function createUser(req, res) {
+    
+    let body = '';
+
+    req.on('data', (chunk) => {
+        body += chunk.toString();
+    });
+
+    req.on('end', () => {
+        try {
+            const userData = JSON.parse(body);
+            console.log(`Received user data: ${userData[1]}`);
+
+            // Set CORS headers
+            res.setHeader('Access-Control-Allow-Origin', '*');
+            res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+            // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+            res.setHeader('Access-Control-Allow-Credentials', true);
+
+            res.end(JSON.stringify(userData));
+        } catch (err) {
+            console.error(`Error parsing JSON: ${err}`);
+            res.end('Error parsing JSON')
+        }
+    });
 }
 
 server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
--- file-editor/front-end/file-editor/src/App.jsx
+++ file-editor/front-end/file-editor/src/App.jsx
@@ -17,13 +17,68 @@ function Button() {
   )
 }
 
+// Controlled username input
+function Form() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  // Need to handle form data differently in React app
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const userData = {
+      username: username,
+      password: password
+    }
+
+    console.log(userData);
+
+    try {
+      const response = await fetch('http://localhost:8080/submit-form', { 
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(userData), // Can only pass strings in the body?
+      });
+      
+      const result = await response.json();
+      console.log('Success:', result);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  }
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+      <label htmlFor="username">Username: </label>
+        <input 
+          type="text"
+          id='username'
+          value={username}
+          onChange={(event) => setUsername(event.target.value)} />
+        <br></br>
+        <label htmlFor="password">Password: </label>
+          <input 
+            type="text"
+            id='password'
+            value={password}
+            onChange={(event) => setPassword(event.target.value)} />
+        <br></br>
+        <button type='submit'>Submit</button>
+      </form>
+    </>
+  )
+}
 
 function App() {
 
   return (
     <>
-      <h1>Click the button to log the current time</h1>
-      <Button />
+      <h1>Enter your details</h1>
+      <Form />
     </>
   )
 }
